fad39f6863e6a4297fec831b328b521155f092e4
VoreXxx <vorex.00@mail.ru>
2023-10-14 13:09:38 +0600

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        int i = 5;
-        System.out.println(i);
+        //Task1
+                String firstName = "Ivan ";
+                String middleName = "Ivanovich ";
+                String lastName = "Ivanov ";
+                String fullName = lastName + firstName + middleName;
+                System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+                //Task2
+                System.out.println("Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+                //Task3
+                String fullName1 = "Иванов Семён Семёнович";
+                String fullName2 = fullName1.replace("ё", "е");
+                System.out.println("Данные Ф. И. О. сотрудника — " + fullName2);
     }
 }
